2cead8139dfe15ad7234bd6a86a75fb9cd028dd1
stxvenbillones-afk <stxvenbillones@gmail.com>
2026-09-15 22:52:57 -0400

    Actualizar about.ts

--- src/commands/general/about.ts
+++ src/commands/general/about.ts
@@ -4,19 +4,32 @@ export default {
     name: "about",
     alias: ["infobot", "sc", "base"],
     category: "general",
-    description: "See a Bots Information",
+    description: "Ver información del bot",
+
     async run({ Chisato, from, message }) {
         const caption =
-            "*「 ABOUT 」*\n\n" +
-            "• Creator : Tobz\n" +
-            "• Team :  𓆩 𝚮ɪᴅᴅᴇɴ 𝐅ɪɴᴅᴇʀ ⁣𓆪 \n" +
-            "• Github :\nhttps://github.com/TobyG74\n" +
-            "• Repostitory :\nhttps://github.com/TobyG74/ChisatoBOT\n" +
-            "• Instagram :\nhttps://instagram.com/ini.tobz\n\n" +
-            "*「 MY TEAM 」*\n\n" +
-            "• Arugaz\nhttps://github.com/arugaz\n" +
-            "• Nugraizy\nhttps://github.com/nugraizy\n" +
-            "• ctOS\nhttps://github.com/alphanum404\n";
+            "╭━━━━━━━━━━━━━━━━━━━━━━╮\n" +
+            "┃   👻  𝗫𝗢𝗥𝗜𝗢𝗡 𝗕𝗢𝗧  👻\n" +
+            "╰━━━━━━━━━━━━━━━━━━━━━━╯\n\n" +
+            "• Nombre : Xorion Bot\n" +
+            "• Powered by : Billones\n" +
+            "• Creador de la base : Tobz\n" +
+            "• Team de la base : 𓆩 𝚮ɪᴅᴅᴇɴ 𝐅ɪɴᴅᴇʀ 𓆪\n\n" +
+            "• Github :\n" +
+            "https://github.com/stxvenbillones-afk\n\n" +
+            "• Repositorio Xorion :\n" +
+            "https://github.com/stxvenbillones-afk/xerion-bot\n\n" +
+            "• Instagram del creador de la base :\n" +
+            "https://instagram.com/ini.tobz\n\n" +
+            "╭┤✦ 𝗖𝗥É𝗗𝗜𝗧𝗢𝗦\n" +
+            "┃\n" +
+            "┃  • Arugaz\n" +
+            "┃  • Nugraizy\n" +
+            "┃  • ctOS\n" +
+            "┃\n" +
+            "╰━━━━━━━━━━━━━━━━━━━━━━╯\n\n" +
+            "        𝗣𝗼𝘄𝗲𝗿𝗲𝗱 𝗯𝘆 𝗕𝗶𝗹𝗹𝗼𝗻𝗲𝘀";
+
         await Chisato.sendText(from, caption, message);
     },
 } satisfies ConfigCommands;
