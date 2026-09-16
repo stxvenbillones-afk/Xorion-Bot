c1eb6df6ca289068215249d7c688c7b0da347531
stxvenbillones-afk <stxvenbillones@gmail.com>
2026-09-15 22:41:36 -0400

    Actualizar config.ts

--- src/commands/owner/config.ts
+++ src/commands/owner/config.ts
@@ -5,43 +5,54 @@ export default {
     name: "config",
     alias: ["botconfig"],
     category: "owner",
-    description: "See Bot Config",
+    description: "Ver configuración del bot",
     isOwner: true,
     async run({ Chisato, from, message, botName }) {
-        const config: Config = JSON.parse(fs.readFileSync("./config.json", "utf-8"));
+        const config: Config = JSON.parse(
+            fs.readFileSync("./config.json", "utf-8")
+        );
+
         const { version } = JSON.parse(
             fs.readFileSync("./package.json", "utf-8")
         );
-        let caption = `*「 CONFIG 」*
 
-★ Owner Number : [`;
-        for (let owner of config.ownerNumber) {
+        let caption = `*「 CONFIGURACIÓN 」*\n\n`;
+
+        caption += `★ Números de Owner: [`;
+        for (const owner of config.ownerNumber) {
             caption += ` ${owner} `;
         }
-        caption += `]\n★ Team Number : [`;
-        for (let team of config.teamNumber) {
-            caption += ` ${team} `;
+
+        caption += `]\n★ Números de Admin: [`;
+
+        for (const admin of config.teamNumber) {
+            caption += ` ${admin} `;
         }
-        caption +=
-            `]\n` +
-            `★ Bot Name : ${botName}\n` +
-            `└「 ${version} 」 Version\n` +
-            `└「 ${config.prefix} 」 Prefix\n` +
-            `★ Stickers :\n` +
-            `└★ Packname : ${config.stickers.packname}\n` +
-            `└★ Authorname : ${config.stickers.author}\n` +
-            `★ Settings :\n` +
-            `└「 ${config.settings.ownerNotifyOnline ? "✅" : "❌"} 」 Bot Online Notification\n` +
-            `└「 ${config.settings.useLimit ? "✅" : "❌"} 」 Use Limit\n` +
-            `└「 ${config.settings.useCooldown ? "✅" : "❌"} 」 Use Cooldown\n` +
-            `└「 ${config.settings.autoReadMessage ? "✅" : "❌"} 」 Auto Read Message\n` +
-            `└「 ${config.settings.autoReadStatus ? "✅" : "❌"} 」 Auto Read Status\n` +
-            `└「 ${config.settings.autoCorrect ? "✅" : "❌"} 」 Auto Correct\n` +
-            `└「 ${config.settings.selfbot ? "✅" : "❌"} 」 Selfbot\n` +
-            `★ Call :\n` +
-            `└「 ${config.call.status} 」 Anti Call\n` +
-            `★ Limit :\n` +
-            `└「 ${config.limit.command} 」 Command\n`;
+
+        caption += `]\n`;
+        caption += `★ Nombre del Bot: ${botName}\n`;
+        caption += `└「 ${version} 」 Versión\n`;
+        caption += `└「 ${config.prefix} 」 Prefijo\n`;
+
+        caption += `★ Stickers:\n`;
+        caption += `└★ Packname: ${config.stickers.packname}\n`;
+        caption += `└★ Autor: ${config.stickers.author}\n`;
+
+        caption += `★ Configuración:\n`;
+        caption += `└「 ${config.settings.ownerNotifyOnline ? "✅" : "❌"} 」 Notificación de Owner conectado\n`;
+        caption += `└「 ${config.settings.useLimit ? "✅" : "❌"} 」 Usar límites\n`;
+        caption += `└「 ${config.settings.useCooldown ? "✅" : "❌"} 」 Usar cooldown\n`;
+        caption += `└「 ${config.settings.autoReadMessage ? "✅" : "❌"} 」 Lectura automática de mensajes\n`;
+        caption += `└「 ${config.settings.autoReadStatus ? "✅" : "❌"} 」 Lectura automática de estados\n`;
+        caption += `└「 ${config.settings.autoCorrect ? "✅" : "❌"} 」 Corrección automática\n`;
+        caption += `└「 ${config.settings.selfbot ? "✅" : "❌"} 」 Selfbot\n`;
+
+        caption += `★ Llamadas:\n`;
+        caption += `└「 ${config.call.status} 」 Anti llamadas\n`;
+
+        caption += `★ Límite:\n`;
+        caption += `└「 ${config.limit.command} 」 Comandos\n`;
+
         await Chisato.sendText(from, caption, message);
     },
 } satisfies ConfigCommands;
